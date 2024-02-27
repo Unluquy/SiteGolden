40d54be7f8c1adb7f25339e5e884eaf808224538
Nathan <67969177+Unluquy@users.noreply.github.com>
2024-02-27 12:12:13 +0100

    Update page.js

--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -362,7 +362,7 @@ const ComponentPage = ( ) => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const imageRef = useRef(null);
     const router = useRouter()
-    console.log(eights[currentIndex+1]);
+    eights[currentIndex+1].map((e)=> (console.log(e)))
     //eights[currentIndex].map((zone, index) => (console.log(zone,index)))
 
 // verify if cursor is clicking an eight
